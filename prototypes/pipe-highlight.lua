f3a174c92a39927848b00b98563347c58a79df95
TheStaplergun <staplergun@gmail.com>
2018-10-26 23:23:48 -0400

    Pipe highlight script/New graphics

--- prototypes/pipe-highlight.lua
+++ prototypes/pipe-highlight.lua
@@ -76,5 +76,81 @@ data:extend {
                 filename = '__PickerPipeTools__/graphics/entity/markers/underground-lines-single-vertical.png'
             }
         }
-    }
+    },
+    merge {
+        data.raw['corpse']['wall-remnants'],
+        {
+            name = 'picker-pipe-marker-ew',
+            icon = '__PickerPipeTools__/graphics/entity/markers/pipe-marker-horizontal.png',
+            time_before_removed = 60 * 20,
+            collision_box = {{0, 0}, {0, 0}},
+            final_render_layer = 'selection-box',
+            animation = {
+                width = 32,
+                height = 32,
+                frame_count = 1,
+                direction_count = 1,
+                --scale = 0.5,
+                shift = {0, -0.5},
+                filename = '__PickerPipeTools__/graphics/entity/markers/pipe-marker-horizontal.png'
+            }
+        }
+    },
+    merge {
+        data.raw['corpse']['wall-remnants'],
+        {
+            name = 'picker-pipe-marker-ns',
+            icon = '__PickerPipeTools__/graphics/entity/markers/pipe-marker-vertical.png',
+            time_before_removed = 60 * 20,
+            collision_box = {{0, 0}, {0, 0}},
+            final_render_layer = 'selection-box',
+            animation = {
+                width = 32,
+                height = 32,
+                frame_count = 1,
+                direction_count = 1,
+                --scale = 0.5,
+                shift = {-0.5, 0},
+                filename = '__PickerPipeTools__/graphics/entity/markers/pipe-marker-vertical.png'
+            }
+        }
+    },
+    merge {
+        data.raw['corpse']['wall-remnants'],
+        {
+            name = 'picker-pipe-marker-good-ew',
+            icon = '__PickerPipeTools__/graphics/entity/markers/pipe-marker-horizontal-good.png',
+            time_before_removed = 60 * 20,
+            collision_box = {{0, 0}, {0, 0}},
+            final_render_layer = 'selection-box',
+            animation = {
+                width = 32,
+                height = 32,
+                frame_count = 1,
+                direction_count = 1,
+                --scale = 0.5,
+                shift = {0, -0.5},
+                filename = '__PickerPipeTools__/graphics/entity/markers/pipe-marker-horizontal-good.png'
+            }
+        }
+    },
+    merge {
+        data.raw['corpse']['wall-remnants'],
+        {
+            name = 'picker-pipe-marker-good-ns',
+            icon = '__PickerPipeTools__/graphics/entity/markers/pipe-marker-vertical-good.png',
+            time_before_removed = 60 * 20,
+            collision_box = {{0, 0}, {0, 0}},
+            final_render_layer = 'selection-box',
+            animation = {
+                width = 32,
+                height = 32,
+                frame_count = 1,
+                direction_count = 1,
+                --scale = 0.5,
+                shift = {-0.5, 0},
+                filename = '__PickerPipeTools__/graphics/entity/markers/pipe-marker-vertical-good.png'
+            }
+        }
+    },
 }
--- scripts/pipe-highlight.lua
+++ scripts/pipe-highlight.lua
@@ -22,11 +22,10 @@ local function show_underground_sprites(event)
     }
     for _, entity in pairs(player.surface.find_entities_filtered(filter)) do
         if entity.type == 'pipe-to-ground' or (entity.type == 'pump' and entity.name == 'underground-mini-pump') then
-            local neighborCounter = 0
             local maxNeighbors = pipe_connections[entity.name] or 2
             for _, entities in pairs(entity.neighbours) do
+                local neighbour_count = #entities
                 for _, neighbour in pairs(entities) do
-                    neighborCounter = neighborCounter + 1
                     if neighbour.type == 'pipe-to-ground' or (neighbour.type == 'pump' and neighbour.name == 'underground-mini-pump') then
                         if (entity.position.x - neighbour.position.x) < -1.5 then
                             local distancex = neighbour.position.x - entity.position.x
@@ -48,12 +47,12 @@ local function show_underground_sprites(event)
                         end
                     end
                 end
-                if (maxNeighbors == neighborCounter) then
+                if (maxNeighbors == neighbour_count) then
                     entity.surface.create_entity {
                         name = 'picker-pipe-marker-box-good',
                         position = entity.position
                     }
-                elseif (neighborCounter < maxNeighbors) then
+                elseif (neighbour_count < maxNeighbors) then
                     entity.surface.create_entity {
                         name = 'picker-pipe-marker-box-bad',
                         position = entity.position
@@ -66,37 +65,149 @@ end
 Event.register('picker-show-underground-paths', show_underground_sprites)
 --? Working on the recursive check.
 
+local function getEW(deltaX)
+    return deltaX > 0 and defines.direction.west or defines.direction.east
+end
+
+local function getNS(deltaY)
+    return deltaY > 0 and defines.direction.north or defines.direction.south
+end
+
+local function get_direction(entity, neighbour)
+    if not entity.valid or not neighbour.valid then
+        return
+    end
+    local deltaX = entity.position.x - neighbour.position.x
+    local deltaY = entity.position.y - neighbour.position.y
+    if deltaX ~= 0 and deltaY == 0 then
+        return getEW(deltaX)
+    elseif deltaX == 0 and deltaY ~= 0 then
+        return getNS(deltaY)
+    elseif deltaX ~= 0 and deltaY ~= 0 then
+        if math.abs(deltaX) > math.abs(deltaY) then
+            return getEW(deltaX)
+        elseif math.abs(deltaX) < math.abs(deltaY) then
+            return getNS(deltaY)
+        end
+    end
+end
+
 local allowed_types =
 {
     ["pipe"] = true,
     ["pipe-to-ground"] = true,
     ["pump"] = true
 }
-local function recurse_pipeline(entity, tracked_entities)
-    local tracked_entities = tracked_entities or {}
-    if not tracked_entities[entity.unit_number] then
-        tracked_entities[entity.unit_number] = entity
-    end
-    for _, entities in pairs(entity.neighbours) do
-        for _, neighbour in pairs(entities) do
-            if allowed_types[neighbour.type] and not tracked_entities[neighbour.unit_number] then
-                tracked_entities[neighbour.unit_number] = neighbour
-                recurse_pipeline(neighbour, tracked_entities)
-            end
+local pipe_highlight_marker = {
+    [defines.direction.north] = 'picker-pipe-marker-ns',
+    [defines.direction.east] = 'picker-pipe-marker-ew',
+    [defines.direction.west] = 'picker-pipe-marker-ew',
+    [defines.direction.south] = 'picker-pipe-marker-ns',
+}
+local pipe_highlight_marker_good = {
+    [defines.direction.north] = 'picker-pipe-marker-good-ns',
+    [defines.direction.east] = 'picker-pipe-marker-good-ew',
+    [defines.direction.west] = 'picker-pipe-marker-good-ew',
+    [defines.direction.south] = 'picker-pipe-marker-good-ns',
+}
+local function draw_underground_sprites(entity, neighbour)
+    local entity_position = entity.position
+    local neighbour_position = neighbour.position
+    local delta_x = entity_position.x - neighbour_position.x
+    local delta_y = entity_position.y - neighbour_position.y
+    if delta_x < -1.5 then
+        local distance_x = neighbour_position.x - entity_position.x
+        for i = 1, distance_x - 1, 1 do
+            entity.surface.create_entity {
+                name = 'picker-underground-pipe-marker-horizontal',
+                position = {entity_position.x + i, entity_position.y}
+            }
+        end
+    elseif delta_x > 1.5 then
+        for i = 1, delta_x - 1, 1 do
+            entity.surface.create_entity {
+                name = 'picker-underground-pipe-marker-horizontal',
+                position = {entity_position.x - i, entity_position.y}
+            }
+        end
+    elseif delta_y < -1.5 then
+        local distance_y = neighbour_position.y - entity_position.y
+        for i = 1, distance_y - 1, 1 do
+            entity.surface.create_entity {
+                name = 'picker-underground-pipe-marker-vertical',
+                position = {entity_position.x, entity_position.y + i}
+            }
+        end
+    elseif delta_y > 1.5 then
+        for i = 1, delta_y - 1, 1 do
+            entity.surface.create_entity {
+                name = 'picker-underground-pipe-marker-vertical',
+                position = {entity_position.x, entity_position.y - i}
+            }
         end
     end
-    return tracked_entities
 end
+
+local function shift_in_direction(current_direction, position, distance_to_shift)
+    if current_direction == defines.direction.north then
+        return {position.x, position.y - distance_to_shift}
+    elseif current_direction == defines.direction.east then
+        return {position.x + distance_to_shift, position.y}
+    elseif current_direction == defines.direction.west then
+        return {position.x - distance_to_shift, position.y}
+    elseif current_direction == defines.direction.south then
+        return {position.x, position.y + distance_to_shift}
+    end
+end
+
 local function highlight_pipeline(event)
     local player, _ = Player.get(event.player_index)
     local selection = player.selected
     if selection and allowed_types[selection.type] then
-        local entities_to_highlight = recurse_pipeline(selection)
-        for _ , entity in pairs(entities_to_highlight) do
-            entity.surface.create_entity{
-                name = 'picker-pipe-marker-box-good',
-                position = entity.position
-            }
+
+        local tracked_entities = {[selection.unit_number] = true}
+        local orphan_counter = 0
+        local args = {position = {}}
+        local function recurse_pipeline(entity)
+            local entity_position = entity.position
+            for _ , entities in pairs(entity.neighbours) do
+                local neighbour_count = #entities
+                for _, neighbour in pairs(entities) do
+                    local current_direction = get_direction(entity, neighbour)
+                    local neighbour_type = neighbour.type
+                    local neighbour_unit_number = neighbour.unit_number
+                    local max_neighbours = pipe_connections[entity.name] or 2
+                    if (neighbour_count < 2 and entity.type == 'pipe') or (neighbour_count < max_neighbours) then
+                        entity.surface.create_entity {
+                            name = 'picker-pipe-marker-box-bad',
+                            position = entity_position
+                        }
+                        orphan_counter = orphan_counter + 1
+                    end
+                    if allowed_types[neighbour_type] and not tracked_entities[neighbour_unit_number] then
+                        tracked_entities[neighbour_unit_number] = true
+                        if entity.type == 'pipe-to-ground' and neighbour_type == 'pipe-to-ground' or (neighbour_type == 'pump' and neighbour.name == 'underground-mini-pump') then
+                            draw_underground_sprites(entity, neighbour)
+                        else
+                            local neighbour_position = neighbour.position
+                            args.position[1] = (entity_position.x + neighbour_position.x)/2
+                            args.position[2] = (entity_position.y + neighbour_position.y)/2
+                            args.name = pipe_highlight_marker[current_direction]
+                            entity.surface.create_entity(args)
+                        end
+                        recurse_pipeline(neighbour)
+                    elseif not allowed_types[neighbour_type] then
+                        local position_to_place = shift_in_direction(current_direction, entity_position, 0.5)
+                        args.position = position_to_place
+                        args.name = pipe_highlight_marker_good[current_direction]
+                        entity.surface.create_entity(args)
+                    end
+                end
+            end
+        end
+        recurse_pipeline(selection)
+        if orphan_counter > 0 then
+            game.print(orphan_counter .. " dead end pipes detected")
         end
     end
 end
