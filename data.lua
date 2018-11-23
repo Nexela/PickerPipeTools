86743362b8083159150ef8bc72021a8dfa2c4c23
kimberly wood <staplergun@gmail.com>
2018-11-23 09:14:20 -0500

    Change to match hotkey migrate

--- data.lua
+++ data.lua
@@ -1,30 +1,3 @@
-data:extend{
-    {
-        type = 'custom-input',
-        name = 'picker-show-underground-paths',
-        key_sequence = 'CONTROL + SHIFT + P',
-        consuming = 'script-only'
-    },
-    {
-        type = "custom-input",
-        name = "picker-toggle-pipe-clamp",
-        key_sequence = "CONTROL + R",
-        --linked_game_control = 'rotate',
-        consuming = "script-only"
-    },
-    {
-        type = "custom-input",
-        name = "picker-auto-clamp-toggle",
-        key_sequence = "CONTROL + SHIFT + C",
-        consuming = "script-only"
-    },
-    --[[{
-        type = "custom-input",
-        name = "picker-highlight-pipeline",
-        key_sequence = "CONTROL + ALT + P",
-        consuming = "script-only"
-    }]]--
-}
-
 require('prototypes/pipe-highlight')
 require('prototypes/pipe-cleaner')
+require('hotkeys')
