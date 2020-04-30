c8c07336d0fa05bd973cb6f13b433245d89f8fda
nexela <nexela@gmail.com>
2020-04-30 14:06:40 -0400

    Remove deprecated custom input type

--- prototypes/controls.lua
+++ prototypes/controls.lua
@@ -3,26 +3,26 @@ data:extend{
         type = 'custom-input',
         name = 'picker-show-underground-paths',
         key_sequence = 'CONTROL + SHIFT + P',
-        consuming = 'script-only'
+        consuming = 'none'
     },
     {
         type = "custom-input",
         name = "picker-toggle-pipe-clamp",
         key_sequence = "CONTROL + R",
         --linked_game_control = 'rotate',
-        consuming = "script-only"
+        consuming = "none"
     },
     -- {
     --     type = "custom-input",
     --     name = "picker-auto-clamp-toggle",
     --     key_sequence = "CONTROL + SHIFT + C",
-    --     consuming = "script-only"
+    --     consuming = "none"
     -- },
     {
         type = "custom-input",
         name = "picker-auto-highlight-toggle",
         key_sequence = "CONTROL + SHIFT + H",
-        consuming = "script-only"
+        consuming = "none"
     },
     {
         type = 'custom-input',
