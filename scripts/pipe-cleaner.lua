69988bf9d619017462a83143558ed7abb3cbd071
Nexela <nexela@gmail.com>
2020-08-30 00:30:42 -0400

    Clamps default to off

--- scripts/pipe-cleaner.lua
+++ scripts/pipe-cleaner.lua
@@ -6,64 +6,69 @@ local Event = require('__stdlib__/stdlib/event/event')
 local table = require('__stdlib__/stdlib/utils/table')
 
 --Start at a drain and clear fluidboxes out that match. find drain connections not cleaned and repeat
-local function call_a_plumber(event)
-    local plumber = game.players[event.player_index]
-    if not (plumber.admin or not settings.global['picker-tool-admin-only'].value) then
+local function flush(event)
+    local player = game.players[event.player_index]
+    local entity = player.selected
+    local fluidbox = entity and entity.fluidbox
+
+    if not (entity and fluidbox) then
         return
     end
 
-    local toilet = plumber.selected
-    local ptrap = toilet and toilet.fluidbox
-    if ptrap then
-        for i = 1, #ptrap do
-            ptrap.flush(i)
+    if not (player.admin or not settings.global['picker-tool-admin-only'].value) then
+        return player.print({'picker.must-be-admin'})
+    end
+
+    if fluidbox then
+        for i = 1, #fluidbox do
+            fluidbox.flush(i)
         end
-        ptrap.owner.last_user = plumber
+        fluidbox.owner.last_user = player
     end
 end
-Event.register('picker-pipe-cleaner', call_a_plumber)
+Event.register('picker-pipe-cleaner', flush)
 
 --API request flush_filters
 local function remove_graffiti(event)
-    local plumber = game.players[event.player_index]
-    if plumber.admin or not settings.global['picker-tool-admin-only'].value then
-        local toilet = plumber.selected
-        if toilet then
-            local frame = plumber.gui.center.picker_pipe_filter
+    local player = game.players[event.player_index]
+    local entity = player.selected
+    local fluidbox = entity and entity.fluidbox
 
-            if frame then
-                frame.destroy()
-            end
-
-            local ptrap = toilet.fluidbox
-            local toilets = {}
-            local rootered = {}
-
-            local function rooter_it(v)
-                if not rootered[v.owner.unit_number] then
-                    toilets[v.owner.unit_number] = v
-                end
-            end
-
-            toilets[toilet.unit_number] = ptrap
-
-            repeat
-                local index, drain = next(toilets)
-                if index then
-                    rootered[index] = drain
-                    for i = 1, #drain do
-                        drain.set_filter(i, nil)
-                        table.each(drain.get_connections(i), rooter_it)
-                    end
-
-                    toilets[index] = nil
-                    drain.owner.last_user = plumber
-                end
-            until not index
-            plumber.print({'pipe-cleaner.remove-all-filters'})
-        end
-    else
-        plumber.print({'picker.must-be-admin'})
+    if not (entity and fluidbox) then
+        return
     end
+
+    if not (player.admin or not settings.global['picker-tool-admin-only'].value) then
+        return player.print({'picker.must-be-admin'})
+    end
+
+    local frame = player.gui.center.picker_pipe_filter
+    local _ = frame and frame.destroy()
+
+    local entities = {}
+    local rootered = {}
+
+    local function rooter_it(v)
+        if not rootered[v.owner.unit_number] then
+            entities[v.owner.unit_number] = v
+        end
+    end
+
+    entities[entity.unit_number] = fluidbox
+
+    repeat
+        local index, drain = next(entities)
+        if index then
+            rootered[index] = drain
+            for i = 1, #drain do
+                drain.set_filter(i, nil)
+                table.each(drain.get_connections(i), rooter_it)
+            end
+
+            entities[index] = nil
+            drain.owner.last_user = player
+        end
+    until not index
+    player.print({'pipe-cleaner.remove-all-filters'})
 end
 Event.register('picker-filter-cleaner', remove_graffiti)
