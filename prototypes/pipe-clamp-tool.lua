14f29fd2511100deea4c509e0f072449461966a3
Nexela <nexela@gmail.com>
2020-10-11 02:24:51 -0400

    Clean up prototype loading.

--- prototypes/pipe-clamp-tool.lua
+++ prototypes/pipe-clamp-tool.lua
@@ -9,7 +9,6 @@ if settings.startup['picker-tool-pipe-clamps'].value then
             subgroup = 'tool',
             order = 'c[selection-tool]-a[pipe-cleaner]',
             stack_size = 1,
-            stackable = false,
             selection_color = {r = 1, g = 0, b = 0},
             alt_selection_color = {r = 0, g = 1, b = 0},
             selection_mode = {'buildable-type', 'items-to-place'},
@@ -17,7 +16,6 @@ if settings.startup['picker-tool-pipe-clamps'].value then
             selection_cursor_box_type = 'copy',
             alt_selection_cursor_box_type = 'copy',
             always_include_tiles = false,
-            show_in_library = true
         }
     }
 end
